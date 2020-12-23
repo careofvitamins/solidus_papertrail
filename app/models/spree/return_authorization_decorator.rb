d35fae6914c0e12655d41c2cdceef340b7bf8736
Scott Bader <sb@scottbader.org>
2020-12-23 17:43:11 -0500

    Updates for new version of papertrail

--- app/models/spree/return_authorization_decorator.rb
+++ app/models/spree/return_authorization_decorator.rb
@@ -1,6 +1,6 @@
 module Spree
   ReturnAuthorization.class_eval do
     include Spree::Versionable
-    has_paper_trail class_name: 'Spree::ReturnAuthorizationVersion', on: %i[create update destroy]
+    has_paper_trail versions: { class_name: 'Spree::ReturnAuthorizationVersion' }, on: %i[create update destroy]
   end
 end
--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -1,6 +1,6 @@
 module Spree
   Shipment.class_eval do
     include Spree::Versionable
-    has_paper_trail class_name: 'Spree::ShipmentVersion', on: %i[create update destroy]
+    has_paper_trail versions: { class_name: 'Spree::ShipmentVersion' }, on: %i[create update destroy]
   end
 end
